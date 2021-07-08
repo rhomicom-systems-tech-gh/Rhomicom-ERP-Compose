24a8dbb0e9fffa4ba9882ae308b78831f851b51e
richardamensah <richarda.mensah@gmail.com>
2021-07-08 02:51:50 +0000

    Change Start/Stop Commands

--- stop-rho_erp.bat
+++ stop-rho_erp.bat
@@ -1,2 +1,3 @@
-docker-compose -p RHO-ERP-SET-1 down
+REM docker-compose -p RHO-ERP-SET-1 down
+docker-compose -p RHO-ERP-SET-1 stop
 REM PAUSE
--- stop-rho_erp.sh
+++ stop-rho_erp.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-docker-compose -p RHO-ERP-SET-1 down
+# docker-compose -p RHO-ERP-SET-1 down
+docker-compose -p RHO-ERP-SET-1 stop
